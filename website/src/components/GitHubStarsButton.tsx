f8ec9e2d8a86d9fdedb8fff4e237b9643722bc02
Rujuta Shinde <rushinde@redhat.com>
2025-07-31 17:34:23 -0400

    fix(website): add github button to the mobile menu
    Signed-off-by: Rujuta Shinde <rushinde@redhat.com>

--- website/src/components/GitHubStarsButton.tsx
+++ website/src/components/GitHubStarsButton.tsx
@@ -3,23 +3,40 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import type { GitHubMetadata } from '@site/src/plugins/github-metadata';
 
+<<<<<<< HEAD
 export function GitHubStarsButton(): JSX.Element {
   const { stargazersCount } = usePluginData('docusaurus-plugin-github-metadata') as GitHubMetadata;
+=======
+export function GitHubStarsButton({ mobile = false }: { readonly mobile?: boolean }): JSX.Element {
+  const stars = useGitHubStars('podman-desktop/podman-desktop');
+>>>>>>> f2bd0cb8e38 (fix(website): add github button to the mobile menu)
 
   return (
     <a
       href="https://github.com/podman-desktop/podman-desktop"
       target="_blank"
-      id="github-stars-button"
       rel="noopener noreferrer"
-      className="hidden lg:flex items-center gap-2 px-4 py-[9px] border border-black dark:border-white rounded-lg navbar__item navbar__link font-medium min-w-[9rem]">
+      // check for mobile or large screen
+      className={
+        mobile
+          ? 'dropdown__link flex items-center gap-2 px-4 py-2 font-medium'
+          : 'navbar__item navbar__link hidden lg:flex items-center gap-2 px-4 py-[9px] border border-black dark:border-white rounded-lg font-medium min-w-[9rem]'
+      }>
       <FontAwesomeIcon icon={faGithub} />
       <span>Star</span>
+<<<<<<< HEAD
       <span
         id="github-stars-badge"
         className="ml-2 px-2 py-1 bg-charcoal-300 rounded text-white text-xs min-w-[2.5rem] text-center">
         {stargazersCount >= 1000 ? `${(stargazersCount / 1000).toFixed(1)}k` : `${stargazersCount}`}
       </span>
+=======
+      {stars && (
+        <span className="ml-2 px-2 py-1 bg-charcoal-300 rounded text-white text-xs min-w-[2.5rem] text-center">
+          {stars}
+        </span>
+      )}
+>>>>>>> f2bd0cb8e38 (fix(website): add github button to the mobile menu)
     </a>
   );
 }
